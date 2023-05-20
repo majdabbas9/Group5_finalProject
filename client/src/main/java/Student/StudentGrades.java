bdd7f73e93e2a4a5871a4f9f37335852dc41649b
m7mdhj <m7.hijazi@gmail.com>
2023-05-20 20:59:55 +0300

    add StudentGrades and edit fxml files

---
+++ client/src/main/java/Student/StudentGrades.java
@@ -0,0 +1,25 @@
+package Student;
+
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.text.Text;
+
+public class StudentGrades {
+
+    @FXML
+    private Button logOutBtn;
+
+    @FXML
+    private Button logOutBtn1;
+
+    @FXML
+    private Text warningtxt;
+
+    @FXML
+    void logOut(ActionEvent event) {
+
+    }
+
+}
