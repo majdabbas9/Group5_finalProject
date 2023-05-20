1cf7dc2b1b9f6852472ff2533c180f3713c132ed
m7mdhj <m7.hijazi@gmail.com>
2023-05-20 16:22:18 +0300

    adding fxmls

---
+++ client/src/main/java/il/cshaifasweng/OCSFMediatorExample/client/StudentHome.java
@@ -0,0 +1,52 @@
+package il.cshaifasweng.OCSFMediatorExample.client;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.text.Text;
+
+public class StudentHome {
+
+    @FXML
+    private Text currentDate;
+
+    @FXML
+    private Text currentTime;
+
+    @FXML
+    private Button doExamBtn;
+
+    @FXML
+    private Button examsCopyBtn1;
+
+    @FXML
+    private Button logOutBtn;
+
+    @FXML
+    private Button nyGradeBtn;
+
+    @FXML
+    private Text studentName;
+
+    @FXML
+    void doExam(ActionEvent event) {
+
+    }
+
+    @FXML
+    void logOut(ActionEvent event) {
+
+    }
+
+    @FXML
+    void showExamsCopy(ActionEvent event) {
+
+    }
+
+    @FXML
+    void showStudentGrade(ActionEvent event) {
+
+    }
+
+}
+
---
+++ client/src/main/java/il/cshaifasweng/OCSFMediatorExample/client/TeacherHome.java
@@ -0,0 +1,67 @@
+package il.cshaifasweng.OCSFMediatorExample.client;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.text.Text;
+
+public class TeacherHome {
+
+    @FXML
+    private Button addQuestionBtn;
+
+    @FXML
+    private Button buildExamBtn;
+
+    @FXML
+    private Text currentDate;
+
+    @FXML
+    private Text currentTime;
+
+    @FXML
+    private Button examsApproveBtn;
+
+    @FXML
+    private Button examsInProgressBtn;
+
+    @FXML
+    private Button logOutBtn;
+
+    @FXML
+    private Button preparExamsBtn;
+
+    @FXML
+    private Text studentName;
+
+    @FXML
+    void ExamsInProgress(ActionEvent event) {
+
+    }
+
+    @FXML
+    void ExamsNeedApprovment(ActionEvent event) {
+
+    }
+
+    @FXML
+    void addQuestions(ActionEvent event) {
+
+    }
+
+    @FXML
+    void buildExam(ActionEvent event) {
+
+    }
+
+    @FXML
+    void logOut(ActionEvent event) {
+
+    }
+
+    @FXML
+    void prepareExamsForExecution(ActionEvent event) {
+
+    }
+
+}
