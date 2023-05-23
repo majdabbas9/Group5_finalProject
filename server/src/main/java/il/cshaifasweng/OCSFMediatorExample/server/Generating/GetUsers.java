c9cab0a5282e16340c8b9fb10f43d94f50da7c24
majd_ <majd.abbas999@gmail.com>
2023-05-23 16:47:17 +0300

    (majd) 23/5/2023 16:47  adam change

--- server/src/main/java/il/cshaifasweng/OCSFMediatorExample/server/Generating/GetUsers.java
+++ server/src/main/java/il/cshaifasweng/OCSFMediatorExample/server/Generating/GetUsers.java
@@ -2,7 +2,28 @@ package il.cshaifasweng.OCSFMediatorExample.server.Generating;
 
 import il.cshaifasweng.OCSFMediatorExample.entities.appUsers.Student;
 import il.cshaifasweng.OCSFMediatorExample.entities.appUsers.Teacher;
+import il.cshaifasweng.OCSFMediatorExample.entities.educational.Course;
+import il.cshaifasweng.OCSFMediatorExample.entities.educational.Subject;
 import org.hibernate.Session;
 
+import javax.persistence.Query;
+import java.util.List;
+
 public class GetUsers {
+    public static List<Subject> getTeacherSubjects(Session session, int id)
+    {
+        String queryString = "FROM Teacher  WHERE id = :id";
+        Query query = session.createQuery(queryString, Teacher.class);
+        query.setParameter("id",id);
+        List<Subject> subs = query.getResultList();
+        return subs;
+    }
+    public static List<Course> getTeacherCourses(Session session, int id)
+    {
+        String queryString = "FROM Teacher  WHERE id = :id";
+        Query query = session.createQuery(queryString, Teacher.class);
+        query.setParameter("id",id);
+        List<Course> list = query.getResultList();
+        return list;
+    }
 }
