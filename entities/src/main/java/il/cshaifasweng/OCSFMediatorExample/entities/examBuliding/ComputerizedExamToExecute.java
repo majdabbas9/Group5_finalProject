246a320b92d6b96cde9cead4d24e196f3074413e
mohammedagbariaa <mhmdjber2@gmail.com>
2023-06-13 15:34:07 +0300

    adding to database

--- entities/src/main/java/il/cshaifasweng/OCSFMediatorExample/entities/examBuliding/ComputerizedExamToExecute.java
+++ entities/src/main/java/il/cshaifasweng/OCSFMediatorExample/entities/examBuliding/ComputerizedExamToExecute.java
@@ -11,7 +11,7 @@ import java.util.List;
 import java.util.Set;
 
 @Entity
-@Table(name = "compExams")
+@Table(name = "compexams")
 public class ComputerizedExamToExecute implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
